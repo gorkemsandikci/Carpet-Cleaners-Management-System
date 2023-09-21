d5bc537aea93344a83cfb92c4a26448c6005b43a
gorkemsandikci <gorkemsandikci@gmail.com>
2023-09-21 17:47:19 +0300

    panel sidebar updated

--- resources/views/backend/inc/sidebar.blade.php
+++ resources/views/backend/inc/sidebar.blade.php
@@ -23,6 +23,34 @@
         </li>
 
         <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#services-nav-item" aria-expanded="false" aria-controls="services-nav-item">
+                <i class="icon-layout menu-icon"></i>
+                <span class="menu-title">Hizmetlerimiz(e)</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="services-nav-item">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="#">Listele(e)</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Oluştur(e)</a></li>
+                </ul>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#gallery-nav-item" aria-expanded="false" aria-controls="gallery-nav-item">
+                <i class="icon-layout menu-icon"></i>
+                <span class="menu-title">Galeri(e)</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="gallery-nav-item">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="#">Listele(e)</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Oluştur(e)</a></li>
+                </ul>
+            </div>
+        </li>
+
+        <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#about-nav-item" aria-expanded="false" aria-controls="about-nav-item">
                 <i class="icon-layout menu-icon"></i>
                 <span class="menu-title">Hakkımızda</span>
@@ -31,6 +59,11 @@
             <div class="collapse" id="about-nav-item">
                 <ul class="nav flex-column sub-menu">
                     <li class="nav-item"> <a class="nav-link" href="{{ route('panel.about.index') }}">Genel</a></li>
+                </ul>
+            </div>
+            <div class="collapse" id="about-nav-item">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="#">İletişim(e)</a></li>
                 </ul>
             </div>
         </li>
